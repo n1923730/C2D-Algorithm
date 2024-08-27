88cc06478a8bfb853da10c4d655d94d55b7823bb
n1923730 <168531674+n1923730@users.noreply.github.com>
2024-08-27 14:13:48 +0200

    hopefully right this time

--- __main__.py
+++ __main__.py
@@ -10,21 +10,6 @@ def get_data():
     if(not os.path.exists(INPUT_FOLDER)): return
      
     onlyfiles = [f for f in os.listdir(INPUT_FOLDER) if os.path.isfile(os.path.join(INPUT_FOLDER, f))]
-    print("os.listdir(): ")
-    for entry in os.listdir(INPUT_FOLDER):
-        print(entry)
-        if os.path.isdir(os.path.join(INPUT_FOLDER, entry)):
-            for entry1 in os.listdir(INPUT_FOLDER + entry):
-                print(" --  " + entry1)
-                if os.path.isdir(os.path.join(INPUT_FOLDER, entry)):
-                    for entry2 in os.listdir(INPUT_FOLDER + entry2):
-                        print("  ----  " + entry2)
-                read_file = open(INPUT_FOLDER + "/" + entry + "/" + entry1 , "r")
-                print(read_file.read())
-        read_file = open(INPUT_FOLDER + "/" + entry , "r")
-        print(read_file.read())    
-
-
 
     match len(onlyfiles):
         case 0:
@@ -56,15 +41,13 @@ def get_data():
 
 def compute_average(data):
     if (data and len(data)):
-        if(type(data) == type(str)):
-            json_data = json.loads(data)
-        else: json_data = data
+        json_data = json.loads(data)
         length = 0
         avg = 0
+        print(type(json_data))
         for point in json_data:
             length += 1
             avg += int(point['value'])
-
         avg = avg / length
         write_file = open(OUTPUT_FOLDER + "/result", 'w')
         write_file.write("{avg: " + str(avg) + "}")
